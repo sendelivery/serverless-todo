e9a112eba2848b0ea866b506844fd819d34f169c
Sebastian Chumaceiro <sebcr99@gmail.com>
2024-01-07 18:58:36 +0000

    Simplified get_items lambda

--- todo-api/lambda/get_items/get_items.py
+++ todo-api/lambda/get_items/get_items.py
@@ -59,22 +59,7 @@ data = [
 
 
 def handler(event, context):
-    func_arn = context.invoked_function_arn
-    func_alias = func_arn.rsplit(":", 1)[-1]
-
-    try:
-        api_stage = event["stageVariables"]["lambdaAlias"]
-    except KeyError:
-        return {"statusCode": 404, "body": json.dumps({"msg": "Invalid API path."})}
-
     return {
         "statusCode": 200,
-        "body": json.dumps(
-            {
-                "msg": f"You've hit the {api_stage} API Stage and the {func_alias} Lambda Alias.",
-                "apiStage": api_stage,
-                "lambdaAlias": func_alias,
-                "data": data,
-            }
-        ),
+        "body": json.dumps({"data": data}),
     }
