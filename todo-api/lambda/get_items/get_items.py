173ca786d3edf9a2d300466bfd7a909b9bf91b67
Sebastian Chumaceiro <sebcr99@gmail.com>
2024-01-07 17:43:02 +0000

    Formatting

--- todo-api/lambda/get_items/get_items.py
+++ todo-api/lambda/get_items/get_items.py
@@ -1,84 +1,80 @@
 import json
 
 data = [
-  {
-    "id": 0,
-    "date": "2023-11-26T10:10:22.296Z",
-    "description": "Finalise Todo app UI and system designs",
-    "completed": True,
-  },
-  {
-    "id": 1,
-    "date": "2023-11-26T10:10:22.296Z",
-    "description": "Implement Todo app frontend",
-    "completed": False,
-  },
-  {
-    "id": 2,
-    "date": "2023-11-26T10:10:22.296Z",
-    "description": "Draft Todo app AWS infrastructure as IaC",
-    "completed": False,
-  },
-  {
-    "id": 3,
-    "date": "2023-11-26T10:10:22.296Z",
-    "description": "Implement CRUD Lambdas for Todo app",
-    "completed": False,
-  },
-  {
-    "id": 4,
-    "date": "2023-11-26T10:10:22.296Z",
-    "description":
-      "Hook up Todo app frontend to backend via API Gateway and Next.js API routes",
-    "completed": False,
-  },
-  {
-    "id": 5,
-    "date": "2023-11-26T10:10:22.296Z",
-    "description": "Run a full test of Todo app",
-    "completed": False,
-  },
-  {
-    "id": 6,
-    "date": "2023-11-26T10:10:22.296Z",
-    "description": "Fix bugs and clean up code",
-    "completed": False,
-  },
-  {
-    "id": 7,
-    "date": "2023-11-26T10:10:22.296Z",
-    "description": "Create documentation for Todo app",
-    "completed": False,
-  },
-  {
-    "id": 8,
-    "date": "2023-11-26T10:10:22.296Z",
-    "description": "Bask in glory!",
-    "completed": False,
-  },
+    {
+        "id": 0,
+        "date": "2023-11-26T10:10:22.296Z",
+        "description": "Finalise Todo app UI and system designs",
+        "completed": True,
+    },
+    {
+        "id": 1,
+        "date": "2023-11-26T10:10:22.296Z",
+        "description": "Implement Todo app frontend",
+        "completed": False,
+    },
+    {
+        "id": 2,
+        "date": "2023-11-26T10:10:22.296Z",
+        "description": "Draft Todo app AWS infrastructure as IaC",
+        "completed": False,
+    },
+    {
+        "id": 3,
+        "date": "2023-11-26T10:10:22.296Z",
+        "description": "Implement CRUD Lambdas for Todo app",
+        "completed": False,
+    },
+    {
+        "id": 4,
+        "date": "2023-11-26T10:10:22.296Z",
+        "description": "Hook up Todo app frontend to backend via API Gateway and Next.js API routes",
+        "completed": False,
+    },
+    {
+        "id": 5,
+        "date": "2023-11-26T10:10:22.296Z",
+        "description": "Run a full test of Todo app",
+        "completed": False,
+    },
+    {
+        "id": 6,
+        "date": "2023-11-26T10:10:22.296Z",
+        "description": "Fix bugs and clean up code",
+        "completed": False,
+    },
+    {
+        "id": 7,
+        "date": "2023-11-26T10:10:22.296Z",
+        "description": "Create documentation for Todo app",
+        "completed": False,
+    },
+    {
+        "id": 8,
+        "date": "2023-11-26T10:10:22.296Z",
+        "description": "Bask in glory!",
+        "completed": False,
+    },
 ]
 
+
 def handler(event, context):
+    func_arn = context.invoked_function_arn
+    func_alias = func_arn.rsplit(":", 1)[-1]
 
-  func_arn = context.invoked_function_arn
-  func_alias = func_arn.rsplit(":", 1)[-1]
+    try:
+        api_stage = event["stageVariables"]["lambdaAlias"]
+    except KeyError:
+        return {"statusCode": 404, "body": json.dumps({"msg": "Invalid API path."})}
 
-  try:
-    api_stage = event["stageVariables"]["lambdaAlias"]
-  except KeyError:
     return {
-      "statusCode": 404,
-      "body": json.dumps({
-        "msg": "Invalid API path."
-      })
+        "statusCode": 200,
+        "body": json.dumps(
+            {
+                "msg": f"You've hit the {api_stage} API Stage and the {func_alias} Lambda Alias.",
+                "apiStage": api_stage,
+                "lambdaAlias": func_alias,
+                "data": data,
+            }
+        ),
     }
-  
-  return {
-    "statusCode": 200,
-    "body": json.dumps({
-      "msg": f"You've hit the {api_stage} API Stage and the {func_alias} Lambda Alias.",
-      "apiStage": api_stage,
-      "lambdaAlias": func_alias,
-      "data": data
-    })
-  }
--- todo-api/todo_api/todo_api_stack.py
+++ todo-api/todo_api/todo_api_stack.py
@@ -4,98 +4,107 @@ from aws_cdk import (
     aws_lambda as _lambda,
     aws_apigateway as apigw,
     aws_iam as iam,
-    Aws, CfnOutput
+    Aws,
+    CfnOutput,
 )
 from constructs import Construct
 
-class TodoApiStack(Stack):
 
+class TodoApiStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # Define a DynamoDB table
         items_table = dynamo.Table(
-            self, "ItemsTable",
+            self,
+            "ItemsTable",
             partition_key=dynamo.Attribute(
-                name="user_id",
-                type=dynamo.AttributeType.STRING
+                name="user_id", type=dynamo.AttributeType.STRING
             ),
             sort_key=dynamo.Attribute(
-                name="date_created",
-                type=dynamo.AttributeType.STRING
-            )
+                name="date_created", type=dynamo.AttributeType.STRING
+            ),
         )
 
         # Define a consumer Lambda function for the GET verb
-        get_items_function = _lambda.Function(self, "GetItems",
+        get_items_function = _lambda.Function(
+            self,
+            "GetItems",
             runtime=_lambda.Runtime.PYTHON_3_9,
             handler="get_items.handler",
-            code=_lambda.Code.from_asset("lambda/get_items")
+            code=_lambda.Code.from_asset("lambda/get_items"),
         )
         get_items_function.add_environment("TABLE_NAME", items_table.table_name)
         items_table.grant_read_data(get_items_function)
 
         # Define a producer Lambda function for the POST / PUT verbs
-        upsert_items_function = _lambda.Function(self, "UpsertItems",
+        upsert_items_function = _lambda.Function(
+            self,
+            "UpsertItems",
             runtime=_lambda.Runtime.PYTHON_3_9,
             handler="upsert_items.handler",
-            code=_lambda.Code.from_asset("lambda/upsert_items")
+            code=_lambda.Code.from_asset("lambda/upsert_items"),
         )
         upsert_items_function.add_environment("TABLE_NAME", items_table.table_name)
         items_table.grant_write_data(upsert_items_function)
 
         # Define the REST API - deploy "latestDeployment" by default
-        api = apigw.RestApi(self, "DeploymentStagesAPI",
-            rest_api_name="todo-api",
-            deploy=True
+        api = apigw.RestApi(
+            self, "DeploymentStagesAPI", rest_api_name="todo-api", deploy=True
         )
         items_resource = api.root.add_resource("items")
 
         # Define GET method for /items
-        items_get_method = items_resource.add_method("GET",
+        items_get_method = items_resource.add_method(
+            "GET",
             authorization_type=apigw.AuthorizationType.IAM,
             integration=apigw.AwsIntegration(
                 service="lambda",
                 region=Aws.REGION,
                 proxy=True,
-                path=f"2015-03-31/functions/{get_items_function.function_arn}/invocations"
-            )
+                path=f"2015-03-31/functions/{get_items_function.function_arn}/invocations",
+            ),
         )
 
         # Define POST method for /items
-        items_post_method = items_resource.add_method("POST",
+        items_post_method = items_resource.add_method(
+            "POST",
             authorization_type=apigw.AuthorizationType.IAM,
             integration=apigw.AwsIntegration(
                 service="lambda",
                 region=Aws.REGION,
                 proxy=True,
-                path=f"2015-03-31/functions/{upsert_items_function.function_arn}/invocations"
-            )
+                path=f"2015-03-31/functions/{upsert_items_function.function_arn}/invocations",
+            ),
         )
-        
+
         # Define PUT method for /items
-        items_put_method = items_resource.add_method("PUT",
+        items_put_method = items_resource.add_method(
+            "PUT",
             authorization_type=apigw.AuthorizationType.IAM,
             integration=apigw.AwsIntegration(
                 service="lambda",
                 region=Aws.REGION,
                 proxy=True,
-                path=f"2015-03-31/functions/{upsert_items_function.function_arn}/invocations"
-            )
+                path=f"2015-03-31/functions/{upsert_items_function.function_arn}/invocations",
+            ),
         )
 
         # Permission to invoke get_items Lambda
-        get_items_function.add_permission("APIGWInvokeGetItemsPermission",
+        get_items_function.add_permission(
+            "APIGWInvokeGetItemsPermission",
             principal=iam.ServicePrincipal("apigateway.amazonaws.com"),
             action="lambda:InvokeFunction",
             source_arn=api.arn_for_execute_api(
                 method=items_get_method.http_method,
                 path=items_resource.path,
-            )
+            ),
         )
 
         # Define outputs
-        CfnOutput(self, "ApiHostUrl",
+        CfnOutput(
+            self,
+            "ApiHostUrl",
             value=f"{api.rest_api_id}.execute-api.{Aws.REGION}.amazonaws.com",
-            description="API Host URL"
+            description="API Host URL",
         )
