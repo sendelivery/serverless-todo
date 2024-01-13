737d4ea2e13030f1c31ef61479ff473bee27d4d3
Sebastian Chumaceiro <sebcr99@gmail.com>
2024-01-13 14:33:07 +0000

    Introduced environment variables for items API

---
+++ todo-web/lib/consts.ts
@@ -0,0 +1,15 @@
+function tryToRetrieveEnvironmentVariables() {
+  const endpoint = process.env.TODO_API_ENDPOINT;
+  const key = process.env.TODO_API_KEY;
+
+  if (!endpoint || !key) {
+    throw new Error(
+      `Unable to retrieve environment variables, TODO_API_ENDPOINT: ${endpoint}, TODO_API_KEY: ${key}`
+    );
+  }
+
+  return [endpoint, key];
+}
+
+export const [todoApiEndpoint, todoApiKey] =
+  tryToRetrieveEnvironmentVariables();
