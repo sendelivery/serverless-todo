8354fbbee76207dc7dcd6de30d7abe7a4729e469
Sebastian Chumaceiro <sebcr99@gmail.com>
2024-03-02 18:23:37 +0000

    Point pipeline to logging branch

--- pipeline/pipeline_stack.py
+++ pipeline/pipeline_stack.py
@@ -24,7 +24,7 @@ class PipelineStack(Stack):
                 input=pipelines.CodePipelineSource.git_hub(
                     # TODO move these into environment variables
                     "sendelivery/serverless-todo",
-                    "main",
+                    "fix/logging",
                 ),
                 commands=[
                     "chmod a+x ./scripts/pipeline/synth",
